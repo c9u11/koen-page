332cc675f0346e97abb16092dd74ff9cc16e8ca8
c9u11 <tjrdud6412@naver.com>
2022-12-11 15:47:48 +0900

    💄 Release
    - release 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Feature from "./components/Feature";
+import Release from "./components/Release";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Feature></Feature>
+      <Release></Release>
     </div>
   );
 }
---
+++ src/components/Release.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+  gap: 10px;
+  padding: 50px 0px;
+`;
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const data = [
+  {
+    version: "pre-release",
+    title: "pre-release open",
+    description: `Global Shortcut 사용
+    Keyboard 제어 라이브러리 사용 (RobotJS)
+    Dragged Text 가져오기
+    영어를 한글로 변환
+    한글을 영어로 변환
+    Build 테스트
+    앱 종료 관련 버그 수정
+    `,
+  },
+];
+
+export default function Release() {
+  return (
+    <Container>
+      {data.map((v) => {
+        return (
+          <Item>
+            <span>{v.version}</span>
+            <h2>{v.title}</h2>
+            <pre>{v.description}</pre>
+          </Item>
+        );
+      })}
+    </Container>
+  );
+}
