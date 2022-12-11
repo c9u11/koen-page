c1e240baa4aeb61d2143391d324447eb525409d3
c9u11 <tjrdud6412@naver.com>
2022-12-11 16:23:35 +0900

    💄 Release text align
    - 디자인 변경

--- src/components/Release.jsx
+++ src/components/Release.jsx
@@ -14,6 +14,10 @@ const Container = styled.div`
 const Item = styled.div`
   display: flex;
   flex-direction: column;
+  text-align: left;
+`;
+const Title = styled.h2`
+  margin-top: 0px;
 `;
 
 const data = [
@@ -21,12 +25,12 @@ const data = [
     version: "pre-release",
     title: "pre-release open",
     description: `Global Shortcut 사용
-    Keyboard 제어 라이브러리 사용 (RobotJS)
-    Dragged Text 가져오기
-    영어를 한글로 변환
-    한글을 영어로 변환
-    Build 테스트
-    앱 종료 관련 버그 수정
+Keyboard 제어 라이브러리 사용 (RobotJS)
+Dragged Text 가져오기
+영어를 한글로 변환
+한글을 영어로 변환
+Build 테스트
+앱 종료 관련 버그 수정
     `,
   },
 ];
@@ -38,7 +42,7 @@ export default function Release() {
         return (
           <Item>
             <span>{v.version}</span>
-            <h2>{v.title}</h2>
+            <Title>{v.title}</Title>
             <pre>{v.description}</pre>
           </Item>
         );
